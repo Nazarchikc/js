f0557adde5759ff5661a7521e4c62ddf5ffd7ef3
Nazarchikc <150057133+Nazarchikc@users.noreply.github.com>
2024-12-09 18:55:38 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+console.log(12);
+const age=14;
+console.log(age);
+
+const name="Nazar";
+console.log(name);
+
+const istSudent=true
+console.log(istSudent);
+
+const myString="Любіть маму їжте кашу любіть Україну"
+console.log(myString);
+
+let myNamber=20;
+myNamber = myNamber + 10 
+console.log(myNamber);
+
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Напиши своє імя")
+alert(userName)
+console.log(typeof userName)
+
+const userAswer = confirm("Ви дійсно хочете покиниути сторінку")
+console.log(userAnswer);
+
+
